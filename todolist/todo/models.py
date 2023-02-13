5510de50be077954a2d322e1db6e075d165ed603
s-t-e-v <steven.bandaogo@proton.me>
2023-02-13 14:36:18 +0100

    basic structure - for loop django

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    checkstate = models.BooleanField(default=False)
+    taskname = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.taskname
--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Todo
 # Create your views here.
+
+def index(request):
+
+    todo = Todo.objects.all()
+
+    context = {"todo": todo}
+
+    return render(request, 'index.html', context)
