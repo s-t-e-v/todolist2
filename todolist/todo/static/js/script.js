89620a9af0869bbdc7acc84132c11fedd5197c4b
s-t-e-v <steven.bandaogo@proton.me>
2023-02-14 11:47:01 +0100

    switch mode js

--- todolist/todo/static/js/script.js
+++ todolist/todo/static/js/script.js
@@ -1,3 +1,5 @@
+// Functions
+
 function add() {
 
     let text_entry = document.getElementById("text_entry");
@@ -7,8 +9,91 @@ function add() {
     }
 }
 
+function switch_mode() {
+
+    // Get swith mode button
+    let switch_mode_button = document.getElementById("trash");
+
+    // Set elements retrieval
+
+        // retrieve all del buttons
+        let del_task_buttons = document.getElementsByClassName("del_task");
+        // retrieve all tasks checkboxes
+        let checkbox = document.getElementsByClassName("checkbox");
+        // retrieve all tasks name
+        let taskname_list = document.getElementsByClassName("taskname");
+
+    // Individual elements retrieval
+
+        // retrieve entry bar
+        let entrybar = document.getElementById("text_entry");
+        // retrieve big button
+        let bigbutton = document.getElementById("big_button");
+
+    if (switch_mode_button.checked) {
+        console.log("deletion mode");
 
 
-document.getElementById("add").addEventListener("click", add);
+        // Change set elements
 
-console.log("arabe")
+            // Display del button
+            for (let i = 0; i < del_task_buttons.length; i++) { // Loop over the collection of elements and set their hidden state
+                del_task_buttons[i].hidden = false;
+            }
+
+            // Hide checkboxes
+            for (let i = 0; i < checkbox.length; i++) { // Loop over the collection of elements and set their hidden state
+                checkbox[i].hidden = true;
+            }
+
+            // Prevent task name modification
+            for (let i = 0; i < taskname_list.length; i++) { // Loop over the collection of elements and set their hidden state
+                taskname_list[i].readOnly = true;
+            }
+
+         // Change Individual elements
+
+            // Disable entry bar
+            entrybar.disabled = true;
+
+            // Change button to "-"
+            bigbutton.innerHTML = "-";
+
+
+    }
+    else {
+        console.log("normal mode");
+
+         // Change set elements
+
+            // Hide del button
+            for (let i = 0; i < del_task_buttons.length; i++) { // Loop over the collection of elements and set their hidden state
+                del_task_buttons[i].hidden = true;
+            }
+
+            // Hide checkboxes
+            for (let i = 0; i < checkbox.length; i++) { // Loop over the collection of elements and set their hidden state
+                checkbox[i].hidden = false;
+            }
+
+            // Allow task name modification
+            for (let i = 0; i < taskname_list.length; i++) { // Loop over the collection of elements and set their hidden state
+                taskname_list[i].readOnly = false;
+            }
+
+         // Change Individual elements
+
+            // Disable entry bar
+            entrybar.disabled = false;
+
+            // Change button to "+"
+            bigbutton.innerHTML = "+";
+
+    }
+}
+
+// Event listener attachment
+
+document.getElementById("big_button").addEventListener("click", add);
+
+document.getElementById("trash").addEventListener("click", switch_mode);
