98490a4be61bb0d281b53fec7077c0241c29fd0d
s-t-e-v <steven.bandaogo@proton.me>
2023-02-14 12:37:44 +0100

    event listener del task buttons

--- todolist/todo/static/js/script.js
+++ todolist/todo/static/js/script.js
@@ -5,11 +5,13 @@ function add() {
     let text_entry = document.getElementById("text_entry");
 
     if (text_entry.value != "") {
-        alert(text_entry.value);
+        console.log("adding elements");
+
     }
 }
 
 function switch_mode() {
+
 
     // Get swith mode button
     let switch_mode_button = document.getElementById("trash");
@@ -92,8 +94,26 @@ function switch_mode() {
     }
 }
 
+function del_task() {
+    console.log("Deleting task");
+}
+
 // Event listener attachment
 
-document.getElementById("big_button").addEventListener("click", add);
+    // Individual elements
 
-document.getElementById("trash").addEventListener("click", switch_mode);
+    document.getElementById("big_button").addEventListener("click", add);
+
+    document.getElementById("trash").addEventListener("click", switch_mode);
+
+
+    // Set elements
+
+    let del_task_buttons = document.getElementsByClassName("del_task");
+
+    for (let i = 0; i < del_task_buttons.length; i++) { // Loop over the collection of elements and assign event listener
+        del_task_buttons[i].addEventListener("click", del_task);
+    }
+
+
+
